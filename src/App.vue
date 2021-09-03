a8002cc8d9e6e1e767bc55687318e6a305374510
laurine herard <laurine.hrd@hotmail.fr>
2021-09-03 12:06:02 +0200

    correction of Category components and add fetch for categories of bdd

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <btn-add-new/>
+    <category-item v-for="c, idx in categories" :key="idx" :category="c"/>
   </div>
 </template>
 
 <script>
+import BtnAddNew from './components/BtnAddNew.vue'
+import CategoryItem from './components/CategoryItem.vue'
 export default {
-  name: 'App'
+  components: { CategoryItem, BtnAddNew },
+  name: 'App',
+  async mounted () {
+    fetch('http://localhost:8741/api/categories').then((response) => {
+      this.categories = response.json().then(json => {
+        this.categories = json['hydra:member']
+      })
+    })
+  },
+  data () {
+    return {
+      categories: []
+    }
+  }
 }
 </script>
 
 <style>
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Gilroy', 'Roboto', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  /* text-align: center; */
+  color: #000000;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="category-item">
+      <p>{{ category.name }}</p>
+      <div class="icon-action">
+          <img
+            src="../assets/pym-iconUpdate.png" 
+            alt="update">
+          <img
+            src="../assets/pym-iconDelete.png" 
+            alt="delete">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryItem',
+  props: {
+    category: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .category-item {
+    background-color: #FCE9E1;
+    padding: 1rem;
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    margin: 1rem 0;
+  }
+  p {
+      font-size: 18px;
+  }
+  .icon-action {
+    display: flex;
+    align-items: center;
+  }
+  img {
+      padding: 0 0.5rem;
+      cursor: pointer;
+  }
+</style>
