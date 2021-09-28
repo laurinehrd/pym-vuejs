709828f05bcd32321b38c298d4ccac3f01162b0e
laurine herard <laurine.hrd@hotmail.fr>
2021-09-28 14:51:30 +0200

    change img to svg for change his color in hover easier

--- src/components/BtnAddNew.vue
+++ src/components/BtnAddNew.vue
@@ -1,6 +1,6 @@
 <template>
   <button>
-      <img src="static/assets/pym-iconMore.png" alt="more">
+      <svg class="add" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17 12H7M12 17V7" stroke-linecap="round"/><path clip-rule="evenodd" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Z"/></svg>
       <p>Ajouter {{ btn }}</p>
   </button>
 </template>
@@ -35,8 +35,12 @@ export default {
       background-color: #F38E69;
       color: white;
   }
-  img {
-      margin-right: 0.5rem;
+  svg.add {
+    stroke: #F38E69;
+    margin-right: 0.5rem;
+  }
+  button:hover svg.add {
+    stroke: white;
   }
   p {
     margin: 0;
