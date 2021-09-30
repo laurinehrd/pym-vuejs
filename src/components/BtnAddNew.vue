7cab85897ac2638a569970c5b87c6f8c3a2b182a
laurine herard <laurine.hrd@hotmail.fr>
2021-09-30 15:14:45 +0200

    add $emit for allow click on this button

--- src/components/BtnAddNew.vue
+++ src/components/BtnAddNew.vue
@@ -1,5 +1,5 @@
 <template>
-  <button>
+  <button @click="$emit('click')">
       <svg class="add" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17 12H7M12 17V7" stroke-linecap="round"/><path clip-rule="evenodd" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Z"/></svg>
       <p>Ajouter {{ btn }}</p>
   </button>
